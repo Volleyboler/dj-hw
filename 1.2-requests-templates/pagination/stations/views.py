71835698605efb8834a03ea78c943e38bb81fafa
Ilia Tregub <forgotten-devil@mail.ru>
2024-02-26 02:21:05 +0500

    added view bus_stations with pagination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,29 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+from django.conf import settings
 
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
+with open(settings.BUS_STATION_CSV, encoding='utf-8', mode='r') as file:
+    reader = csv.DictReader(file)
+    CONTENT = list(reader)
+
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
+    page_number = int(request.GET.get("page", 1))
+    paginator = Paginator(CONTENT, 10)
+    page = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
