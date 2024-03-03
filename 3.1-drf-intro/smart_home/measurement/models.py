874fc34bb9346aabb3c49278f83325fed9766627
Ilia Tregub <forgotten-devil@mail.ru>
2024-03-04 04:20:28 +0500

    added changes

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+
+
+class Measurement(models.Model):
+    sensor_id = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='scopes')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField()
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,24 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import SensorSerializer, SensorDetailSerializer
+from .models import Sensor, Measurement
+
+
+class SensorsAPIView(ListAPIView):
+    sensors = Sensor.objects.all()
+    ser = SensorSerializer(sensors, many=True)
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class RetrieveUpdateSensorAPIView(RetrieveUpdateAPIView):
+    pass
+
+
+class CreateAPIView(APIView):
+    def get(self, request):
+
+        return Response
